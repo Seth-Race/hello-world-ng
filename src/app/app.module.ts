16b5c6ea26a7b7eb988c79ea9ea3cddff619c8b2
Seth-Race <sethrace@gmail.com>
2021-08-06 09:03:32 -0400

    changed small bits

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeyThereComponent } from './feature/hey-there/hey-there.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeyThereComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
